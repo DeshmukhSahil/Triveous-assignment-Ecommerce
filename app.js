3421fda83a9c54c3802980c810ad2ad6c33bc686
Sahil Deshmukh <>
2023-11-09 13:13:44 +0530

    testing express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("hello API");
+})
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000");
+})
