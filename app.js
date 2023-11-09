47cd007bf60b8d3d7231779047be516beea93d52
Sahil Deshmukh <>
2023-11-09 19:07:09 +0530

    adding categories routes and model

--- app.js
+++ app.js
@@ -6,50 +6,58 @@ const morgan =require('morgan');
 const mongoose = require('mongoose');
 
 // middleware
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema= mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
+// Routes
 
-const Product =mongoose.model('Product', productSchema);
+const categoriesRoutes= require('./routes/categories');
+
+// const productSchema= mongoose.Schema({
+//     name: String,
+//     image: String,
+//     countInStock: Number
+// })
+
+// const Product =mongoose.model('Product', productSchema);
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req, res)=>{
-    const product={
-        id:1,
-        name:"hair dresser",
-        image:'any_img',
-    }
-    res.send(product);
-})
+app.use(`${api}/categories`, categoriesRoutes);
 
-app.post(`${api}/products`, (req, res)=>{
-    const product=new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
 
-    })
+// app.get(`${api}/products`, (req, res)=>{
+//     const product={
+//         id:1,
+//         name:"hair dresser",
+//         image:'any_img',
+//     }
+//     res.send(product);
+// })
 
-    product.save().then((createdProduct=>{
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
+// app.post(`${api}/products`, (req, res)=>{
+//     const product=new Product({
+//         name: req.body.name,
+//         image: req.body.image,
+//         countInStock: req.body.countInStock
 
-    })
+//     })
+
+//     product.save().then((createdProduct=>{
+//         res.status(201).json(createdProduct)
+//     })).catch((err)=>{
+//         res.status(500).json({
+//             error: err,
+//             success: false
+//         })
+
+//     })
     
-    // res.send(newProduct);
-})
+//     // res.send(newProduct);
+// })
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
