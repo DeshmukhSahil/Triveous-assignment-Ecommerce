29c6643acaed01382d00715ac577e1fd013a5a3c
Sahil Deshmukh <>
2023-11-10 00:21:23 +0530

    fixing errors and adding jwt & user model,routes

--- models/product.js
+++ models/product.js
@@ -1,69 +1,69 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
-const productSchema=mongoose.Schema({
-    name:{
+const productSchema = mongoose.Schema({
+    name: {
         type: String,
         required: true,
     },
-    description:{
-        type:String,
-        required:true,
+    description: {
+        type: String,
+        required: true
     },
-    richDescription:{
-        type:String,
-        default:"",
+    richDescription: {
+        type: String,
+        default: ''
     },
-    image:{
-        type:String,
-        default:'',
+    image: {
+        type: String,
+        default: ''
     },
-    images:[{
-        type:String
+    images: [{
+        type: String
     }],
-    brand:{
-        type:String,
-        default:''
+    brand: {
+        type: String,
+        default: ''
     },
-    price:{
-        type:Number,
+    price : {
+        type: Number,
         default:0
     },
-    category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Category',
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
         required:true
     },
-    countInStock:{
-        type:Number,
-        required:true,
-        min:0,
-        max:255
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
     },
-    rating:{
-        type:Number,
-        default:0,
+    rating: {
+        type: Number,
+        default: 0,
     },
-    numReviews:{
-        type:Number,
-        default:0,
+    numReviews: {
+        type: Number,
+        default: 0,
     },
-    isFeatured:{
-        type:Boolean,
-        default:false,
+    isFeatured: {
+        type: Boolean,
+        default: false,
     },
-    dateCreated:{
+    dateCreated: {
         type: Date,
-        default:Date.now,
+        default: Date.now,
     },
-
 })
 
-productSchema.virtual('id').get(function(){
+productSchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
 
-productSchema.set('toJSON',{
-    virtuals:true,
-})
+productSchema.set('toJSON', {
+    virtuals: true,
+});
 
-exports.Product=mongoose.model('Product',productSchema);
+
+exports.Product = mongoose.model('Product', productSchema);
