2be3ec29f0f32b1568a1882268654072a38acb67
Sahil Deshmukh <>
2023-11-13 11:16:52 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -124,17 +124,18 @@ router.post('/register', async (req,res)=>{
 })
 
 
-router.delete('/:id', (req, res)=>{
-    User.findByIdAndRemove(req.params.id).then(user =>{
-        if(user) {
-            return res.status(200).json({success: true, message: 'the user is deleted!'})
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.params.id });
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is deleted!' });
         } else {
-            return res.status(404).json({success: false , message: "user not found!"})
+            return res.status(404).json({ success: false, message: 'user not found!' });
         }
-    }).catch(err=>{
-       return res.status(500).json({success: false, error: err}) 
-    })
-})
+    } catch (err) {
+        return res.status(500).json({ success: false, error: err });
+    }
+});
 
 router.get(`/get/count`, async (req, res) =>{
     const userCount = await User.countDocuments();
